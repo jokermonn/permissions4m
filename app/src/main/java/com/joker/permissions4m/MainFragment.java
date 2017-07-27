fa527532a76d0f506b42223b3920fa83e3e3675a
jokermonn <429094465@qq.com>
2017-07-27 13:23:22 +0800

    refactor code

--- app/src/main/java/com/joker/permissions4m/MainFragment.java
+++ app/src/main/java/com/joker/permissions4m/MainFragment.java
@@ -1,12 +1,15 @@
 package com.joker.permissions4m;
 
+
 import android.Manifest;
+import android.app.Fragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.joker.annotation.PermissionsCustomRationale;
@@ -16,7 +19,11 @@ import com.joker.annotation.PermissionsRationale;
 import com.joker.api.Permissions4M;
 import com.joker.permissions4m.other.ToastUtil;
 
-public class MainActivity extends AppCompatActivity {
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragment extends Fragment {
     private static final int STORAGE_CODE = 1;
     private static final int CALL_CODE = 2;
     private static final int CONTACT_CODE = 3;
@@ -24,28 +31,32 @@ public class MainActivity extends AppCompatActivity {
     private static final int SMS_CODE = 5;
     private static final int AUDIO_CODE = 6;
     private Button mCallButton;
-    private Button mStorageButton;
     private Button mContactsButton;
+    private Button mStorageButton;
     private Button mCameraButton;
     private Button mSmsButton;
     private Button mAudioButton;
 
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mCallButton = (Button) findViewById(R.id.btn_call);
-        mContactsButton = (Button) findViewById(R.id.btn_contacts);
-        mStorageButton = (Button) findViewById(R.id.btn_storage);
-        mCameraButton = (Button) findViewById(R.id.btn_camera);
-        mSmsButton = (Button) findViewById(R.id.btn_sms);
-        mAudioButton = (Button) findViewById(R.id.btn_audio);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        mCallButton = (Button) view.findViewById(R.id.btn_call);
+        mContactsButton = (Button) view.findViewById(R.id.btn_contacts);
+        mStorageButton = (Button) view.findViewById(R.id.btn_storage);
+        mCameraButton = (Button) view.findViewById(R.id.btn_camera);
+        mSmsButton = (Button) view.findViewById(R.id.btn_sms);
+        mAudioButton = (Button) view.findViewById(R.id.btn_audio);
 
         // 单个申请
         mContactsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Permissions4M.requestPermission(MainActivity.this, Manifest.permission.READ_CONTACTS,
+                Permissions4M.requestPermission(MainFragment.this, Manifest.permission.READ_CONTACTS,
                         CONTACT_CODE);
             }
         });
@@ -54,14 +65,14 @@ public class MainActivity extends AppCompatActivity {
         mCallButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Permissions4M.requestPermission(MainActivity.this, Manifest.permission.CALL_PHONE,
+                Permissions4M.requestPermission(MainFragment.this, Manifest.permission.CALL_PHONE,
                         CALL_CODE);
             }
         });
         mStorageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Permissions4M.requestPermission(MainActivity.this, Manifest.permission
+                Permissions4M.requestPermission(MainFragment.this, Manifest.permission
                                 .WRITE_EXTERNAL_STORAGE,
                         STORAGE_CODE);
             }
@@ -71,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
         mCameraButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Permissions4M.requestPermission(MainActivity.this, Manifest.permission.CAMERA, CAMERA_CODE);
+                Permissions4M.requestPermission(MainFragment.this, Manifest.permission.CAMERA, CAMERA_CODE);
             }
         });
 
@@ -79,22 +90,24 @@ public class MainActivity extends AppCompatActivity {
         mSmsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Permissions4M.requestPermission(MainActivity.this, Manifest.permission.READ_SMS, SMS_CODE);
+                Permissions4M.requestPermission(MainFragment.this, Manifest.permission.READ_SMS, SMS_CODE);
             }
         });
         mAudioButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Permissions4M.requestPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO,
+                Permissions4M.requestPermission(MainFragment.this, Manifest.permission.RECORD_AUDIO,
                         AUDIO_CODE);
             }
         });
+
+        return view;
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[]
             grantResults) {
-        Permissions4M.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
+        Permissions4M.onRequestPermissionsResult(MainFragment.this, requestCode, permissions, grantResults);
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
@@ -103,10 +116,10 @@ public class MainActivity extends AppCompatActivity {
     public void storageAndCallGranted(int code) {
         switch (code) {
             case STORAGE_CODE:
-                ToastUtil.show("设备存储权限授权成功 in activity");
+                ToastUtil.show("设备存储权限授权成功 in fragment");
                 break;
             case CALL_CODE:
-                ToastUtil.show("通话权限授权成功 in activity");
+                ToastUtil.show("通话权限授权成功 in fragment");
                 break;
         }
     }
@@ -115,10 +128,10 @@ public class MainActivity extends AppCompatActivity {
     public void storageAndCallDenied(int code) {
         switch (code) {
             case STORAGE_CODE:
-                ToastUtil.show("设备存储权限授权失败 in activity");
+                ToastUtil.show("设备存储权限授权失败 in fragment");
                 break;
             case CALL_CODE:
-                ToastUtil.show("通话权限授权失败 in activity");
+                ToastUtil.show("通话权限授权失败 in fragment");
                 break;
         }
     }
@@ -127,10 +140,10 @@ public class MainActivity extends AppCompatActivity {
     public void storageAndCallRationale(int code) {
         switch (code) {
             case STORAGE_CODE:
-                ToastUtil.show("请开启设备存储权限授权 in activity");
+                ToastUtil.show("请开启设备存储权限授权 in fragment");
                 break;
             case CALL_CODE:
-                ToastUtil.show("请开启通话权限授权 in activity");
+                ToastUtil.show("请开启通话权限授权 in fragment");
                 break;
         }
     }
@@ -138,43 +151,43 @@ public class MainActivity extends AppCompatActivity {
     //====================================================================
     @PermissionsGranted(CONTACT_CODE)
     public void contactGranted() {
-        ToastUtil.show("读取联系人权限成功 in activity");
+        ToastUtil.show("读取联系人权限成功 in fragment");
     }
 
     @PermissionsDenied(CONTACT_CODE)
     public void contactDenied() {
-        ToastUtil.show("读取联系人权限失败 in activity");
+        ToastUtil.show("读取联系人权限失败 in fragment");
     }
 
     @PermissionsRationale(CONTACT_CODE)
     public void contactRationale() {
-        ToastUtil.show("请开启读取联系人权限 in activity");
+        ToastUtil.show("请开启读取联系人权限 in fragment");
     }
 
     //====================================================================
     @PermissionsGranted(CAMERA_CODE)
     public void cameraGranted() {
-        ToastUtil.show("相机权限授权成功 in activity");
+        ToastUtil.show("相机权限授权成功 in fragment");
     }
 
     @PermissionsDenied(CAMERA_CODE)
     public void cameraDenied() {
-        ToastUtil.show("相机权限授权失败 in activity");
+        ToastUtil.show("相机权限授权失败 in fragment");
     }
 
     @PermissionsCustomRationale(CAMERA_CODE)
     public void cameraCustomRationale() {
-        new AlertDialog.Builder(this)
-                .setMessage("相机权限申请：\n我们需要您开启相机信息权限(in activity)")
+        new AlertDialog.Builder(getActivity())
+                .setMessage("相机权限申请：\n我们需要您开启相机信息权限(in fragment)")
                 .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        // 请自行处理申请权限，两者方法等价
-                        // 方法1.使用框架封装方法
-                        Permissions4M.requestPermissionOnCustomRationale(MainActivity.this, new String[]{Manifest
+                        // 请自行处理申请权限，两种方法等价
+                        // 方法1，使用框架已封装的方法
+                        Permissions4M.requestPermissionOnCustomRationale(MainFragment.this, new String[]{Manifest
                                 .permission.CAMERA}, CAMERA_CODE);
-                        // 方法2.使用自身方法
-//                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest
+                        // 方法2，使用自身方法
+//                        ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest
 //                                .permission.CAMERA}, CAMERA_CODE);
                     }
                 })
@@ -186,10 +199,10 @@ public class MainActivity extends AppCompatActivity {
     public void smsAndAudioGranted(int code) {
         switch (code) {
             case SMS_CODE:
-                ToastUtil.show("短信权限申请成功 in activity");
+                ToastUtil.show("短信权限申请成功 in fragment");
                 break;
             case AUDIO_CODE:
-                ToastUtil.show("录音权限申请成功 in activity");
+                ToastUtil.show("录音权限申请成功 in fragment");
                 break;
             default:
                 break;
@@ -200,10 +213,10 @@ public class MainActivity extends AppCompatActivity {
     public void smsAndAudioDenied(int code) {
         switch (code) {
             case SMS_CODE:
-                ToastUtil.show("短信权限申请失败 in activity");
+                ToastUtil.show("短信权限申请失败 in fragment");
                 break;
             case AUDIO_CODE:
-                ToastUtil.show("录音权限申请失败 in activity");
+                ToastUtil.show("录音权限申请失败 in fragment");
                 break;
             default:
                 break;
@@ -214,35 +227,39 @@ public class MainActivity extends AppCompatActivity {
     public void smsAndAudioCustomRationale(int code) {
         switch (code) {
             case SMS_CODE:
-                new AlertDialog.Builder(this)
-                        .setMessage("短信权限申请：\n我们需要您开启短信权限(in activity)")
+                new AlertDialog.Builder(getActivity())
+                        .setMessage("短信权限申请：\n我们需要您开启短信权限(in fragment)")
                         .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                // 请自行处理申请权限，两者方法等价
-                                // 方法1.使用框架封装方法
-                                Permissions4M.requestPermissionOnCustomRationale(MainActivity.this, new String[]{Manifest
+                                // 请自行处理申请权限，两种方法等价
+                                // 方法1，使用框架已封装的方法
+                                Permissions4M.requestPermissionOnCustomRationale(MainFragment.this, new String[]{Manifest
                                         .permission.READ_SMS}, SMS_CODE);
-                                // 方法2.使用自身方法
-//                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest
-//                                        .permission.READ_SMS}, SMS_CODE);
+                                // 方法2，使用自身方法
+//                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                                    MainFragment.this.requestPermissions(new String[]{Manifest
+//                                            .permission.READ_SMS}, SMS_CODE);
+//                                }
                             }
                         })
                         .show();
                 break;
             case AUDIO_CODE:
-                new AlertDialog.Builder(this)
-                        .setMessage("录音权限申请：\n我们需要您开启录音权限(in activity)")
+                new AlertDialog.Builder(getActivity())
+                        .setMessage("录音权限申请：\n我们需要您开启录音权限(in fragment)")
                         .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                // 请自行处理申请权限，两者方法等价
-                                // 方法1.使用框架封装方法
-                                Permissions4M.requestPermissionOnCustomRationale(MainActivity.this, new String[]{Manifest
+                                // 请自行处理申请权限，两种方法等价
+                                // 方法1，使用框架已封装的方法
+                                Permissions4M.requestPermissionOnCustomRationale(MainFragment.this, new String[]{Manifest
                                         .permission.RECORD_AUDIO}, AUDIO_CODE);
-                                // 方法2.使用自身方法
-//                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest
-//                                        .permission.RECORD_AUDIO}, AUDIO_CODE);
+                                // 方法2，使用自身方法
+//                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//                                    MainFragment.this.requestPermissions(new String[]{Manifest
+//                                            .permission.RECORD_AUDIO}, AUDIO_CODE);
+//                                }
                             }
                         })
                         .show();
--- permissions4m-api/src/main/java/com/joker/api/Permissions4M.java
+++ permissions4m-api/src/main/java/com/joker/api/Permissions4M.java
@@ -1,5 +1,6 @@
 package com.joker.api;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -16,28 +17,55 @@ public class Permissions4M {
     private static final String PERMISSIONS_PROXY = "$$PermissionsProxy";
     private static PermissionsProxy instance;
 
-    @SuppressWarnings("unchecked")
-    public static void requestPermission(Object object, String permission, int requestCode) {
-        init(object);
+    public static void requestPermission(Activity activity, String permission, int requestCode) {
+        init(activity);
 
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             return;
         }
+        request(activity, permission, requestCode);
+    }
 
-        if (object instanceof Activity) {
-            requestPermission((Activity) object, permission, requestCode);
-        } else if (object instanceof android.support.v4.app.Fragment) {
-            requestPermission((android.support.v4.app.Fragment) object, permission, requestCode);
-        } else if (object instanceof android.app.Fragment) {
-            requestPermission((android.app.Fragment) object, permission, requestCode);
-        } else {
-            throw new IllegalArgumentException(object.getClass().getName() + " is not supported!");
+    public static void requestPermission(android.app.Fragment fragment, String permission, int
+            requestCode) {
+        init(fragment);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return;
         }
+        request(fragment, permission, requestCode);
+    }
+
+    public static void requestPermission(android.support.v4.app.Fragment fragment, String permission, int
+            requestCode) {
+        init(fragment);
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return;
+        }
+        request(fragment, permission, requestCode);
+    }
+
+    public static void requestPermissionOnCustomRationale(Activity activity, String[]
+            permissions, int requestCode) {
+        ActivityCompat.requestPermissions(activity, permissions, requestCode);
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public static void requestPermissionOnCustomRationale(android.app.Fragment fragment, String[]
+            permissions, int requestCode) {
+        fragment.requestPermissions(permissions, requestCode);
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public static void requestPermissionOnCustomRationale(android.support.v4.app.Fragment fragment, String[]
+            permissions, int requestCode) {
+        fragment.requestPermissions(permissions, requestCode);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @SuppressWarnings("unchecked")
-    private static void requestPermission(Activity activity, String permission, int requestCode) {
+    private static void request(Activity activity, String permission, int requestCode) {
         if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
             if (activity.shouldShowRequestPermissionRationale(permission)) {
                 if (!instance.customRationale(activity, requestCode)) {
@@ -54,22 +82,29 @@ public class Permissions4M {
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @SuppressWarnings("unchecked")
-    private static void requestPermission(android.support.v4.app.Fragment fragment, String permission,
-                                          int requestCode) {
+    private static void request(android.app.Fragment fragment, String permission, int requestCode) {
         if (fragment.shouldShowRequestPermissionRationale(permission)) {
-            instance.rationale(fragment, requestCode);
+            if (!instance.customRationale(fragment, requestCode)) {
+                instance.rationale(fragment, requestCode);
+                fragment.requestPermissions(new String[]{permission}, requestCode);
+            }
+        } else {
+            fragment.requestPermissions(new String[]{permission}, requestCode);
         }
-        fragment.requestPermissions(new String[]{permission}, requestCode);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @SuppressWarnings("unchecked")
-    private static void requestPermission(android.app.Fragment fragment, String permission, int
-            requestCode) {
+    private static void request(android.support.v4.app.Fragment fragment, String permission,
+                                int requestCode) {
         if (fragment.shouldShowRequestPermissionRationale(permission)) {
-            instance.rationale(fragment, requestCode);
+            if (!instance.customRationale(fragment, requestCode)) {
+                instance.rationale(fragment, requestCode);
+                fragment.requestPermissions(new String[]{permission}, requestCode);
+            }
+        } else {
+            fragment.requestPermissions(new String[]{permission}, requestCode);
         }
-        fragment.requestPermissions(new String[]{permission}, requestCode);
     }
 
     private static void init(Object object) {
@@ -79,19 +114,10 @@ public class Permissions4M {
             instance = (PermissionsProxy) Class.forName(proxyName).newInstance();
         } catch (InstantiationException e) {
             e.printStackTrace();
-            throw new RuntimeException(String.format("can not compile %s , something wrong when compiler" +
-                    ".", object
-                    .getClass().getSimpleName() + PERMISSIONS_PROXY));
         } catch (IllegalAccessException e) {
             e.printStackTrace();
-            throw new RuntimeException(String.format("can not compile %s , something wrong when compiler" +
-                    ".", object
-                    .getClass().getSimpleName() + PERMISSIONS_PROXY));
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
-            throw new RuntimeException(String.format("can not compile %s , something wrong when compiler" +
-                    ".", object
-                    .getClass().getSimpleName() + PERMISSIONS_PROXY));
         }
     }
 
