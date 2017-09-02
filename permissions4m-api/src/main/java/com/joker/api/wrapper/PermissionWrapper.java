c29cc1a16aabf4676ff2666f8ab3d7ceac3bbd0a
jokermonn <429094465@qq.com>
2017-09-02 19:37:19 +0800

    add an non-public interface

--- permissions4m-api/src/main/java/com/joker/api/wrapper/PermissionWrapper.java
+++ permissions4m-api/src/main/java/com/joker/api/wrapper/PermissionWrapper.java
@@ -4,5 +4,12 @@ package com.joker.api.wrapper;
  * Created by joker on 2017/9/2.
  */
 
-public interface PermissionWrapper {
+public interface PermissionWrapper extends Wrapper {
+    String getRequestPermission();
+
+    int getRequestCode();
+
+    Wrapper requestPermission(String permission);
+
+    Wrapper requestCode(int code);
 }
